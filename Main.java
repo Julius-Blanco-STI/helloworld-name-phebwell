de75680e07012bfbf01639f41c94758220ce5eaa
phebwell <58575980+phebwell@users.noreply.github.com>
2019-12-06 08:34:17 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+
+package main;
+
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+  
+    public static void main(String[] args) {
+    String name = "Lowell Latosa";
+    System.out.println("Hello World "+ name);
+    
+    }
+    
 }
