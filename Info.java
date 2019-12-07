a4749427a8d8e0e69ce141d2beef65e16bd43a25
phebwell <58575980+phebwell@users.noreply.github.com>
2019-12-07 15:12:40 -0800

    Update Info.java

--- Info.java
+++ Info.java
@@ -1,27 +1,24 @@
-
 package info;
 import java.util.Scanner;
 
 public class Info {
-
-
-    public static void main(String[] args) {
-    Scanner scn = new Scanner (System.in);
+    
+   public static void main(String[] args) {
+   Scanner scn = new Scanner (System.in);
    System.out.println("Hello");
+       
    System.out.println("What is your name?");
-   String name = scn.nextLine();
-   System.out.println("you name is " + name);
+       String name = scn.nextLine();
+             System.out.println("you name is " + name);
    
    System.out.println("How old are you?");
-   String age = scn.nextLine();
-   System.out.println ("you are is "+ age);
+       String age = scn.nextLine();
+            System.out.println ("you are is "+ age);
    
    System.out.println ("What is you address?");
-   String address = scn.nextLine ();
-   System.out.println (" your address is " + address);
+       String address = scn.nextLine ();
+            System.out.println (" your address is " + address);
    
     }
     
 }
-
-
